b3f263a3006ee9a1fe6869277f856849a196b08c
Saurabh <sauravthakulla683@gmail.com>
2025-10-21 21:29:49 +0530

    Memory GAme

--- props/src/App.jsx
+++ props/src/App.jsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [grid, setGrid] = useState(4);
+  const [card, setCard] = useState([]);
+  const [flipped, setflipped] = useState([]);
+  const [solved, setsolved] = useState([]);
+  const [disable, setdisable] = useState(false);
+  const [won, setwon] = useState(false);
+
+  const gridchange = (e) => {
+    setGrid(e.target.value);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="flex w-full h-screen flex-col gap-6 justify-center items-center ">
+      <h1 className="text-4xl">Memory Game</h1>
+      {/* INPUT */}
+      <div className="flex justify-center items-center flex-col text-3xl">
+        <label htmlFor="grid">Enter the grid size Max:(10)</label>
+        <input
+          id="grid"
+          type="number"
+          value={grid}
+          onChange={gridchange}
+          className="w-16"
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
 
-export default App
+      {/* Game */}
+
+      {/* WIn pop up */}
+
+      {/* Reset */}
+    </div>
+  );
+};
+
+export default App;
