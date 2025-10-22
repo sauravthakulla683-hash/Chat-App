fa1efb945d4b4e283556e4ae9fef29c3b197ae49
Saurabh <sauravthakulla683@gmail.com>
2025-10-22 22:02:38 +0530

    Backend Started

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config();
+const cors = require("cors");
+const http = require("http");
+
+const app = express();
+const server = http.createServer(app);
+
+app.use(express.json({ limit: "4mb" }));
+app.use(cors());
+
+app.use("/api/status", (req, res) => {
+  res.send("server is running");
+});
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => {
+  console.log(`server is running on ${PORT}`);
+});
